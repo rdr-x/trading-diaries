092d213697ade486317133645c840e37d6de66b2
8pus <rodrx20@gmail.com>
2021-11-14 11:09:58 -0600

    first commit and config files

---
+++ src/index.js
@@ -0,0 +1,43 @@
+/**
+ * FRONTEND
+ * 1. pantalla de inicio
+ * navbar con logo
+ * carrousell de imágenes y un side text
+ * login/signin en una esquina
+ * 
+ * 2. pantalla de login
+ * navbar con logo
+ * tarjeta-contenedor para poner datos de inicio de sesion
+ * 
+ * 
+ * 3. pantalla de signin
+ * navbar con logo 
+ * tarjeta-contenedor para datos de registro
+ * 
+ * 4. pantalla principal
+ * navbar con logo 
+ * si hay trades, mostrar el último
+ * si no, contenedor con un icono para agregar un trade
+ * trade:
+ * imagen o video del trade
+ * contenedor al lado para leer datos (fecha, cantidad, tipo de trade, riesgo, target, fee)
+ * 
+ * BACKEND
+ * Comportamiento
+ * crear clases:
+ *  a) user
+ *  b) mediaplayer
+ *  c) trade
+ * 1. Servidor y Routing con Express.js
+ * solicitudes API REST a la bd
+ * 2. BD
+ * Conectar bd a las instancias user, mediaplayer, trade
+ * 
+ * 3. Autenticación
+ * Web2
+ * Passport
+ * 
+ * Web3
+ * web3.js 
+ * conectar a wallet
+ */
